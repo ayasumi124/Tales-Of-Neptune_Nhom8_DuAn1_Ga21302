5aeb03092a53445f4ee3bb80d99f852db3ff3870
Phuc Nguyen <nnphucnguyen12@gmail.com>
2026-07-14 03:12:38 +0700

    update tilemap

--- Assets/Scripts/Skills/CloneSkill.cs
+++ Assets/Scripts/Skills/CloneSkill.cs
@@ -1,12 +1,44 @@
 using UnityEngine;
+using TMPro;
 
 public class CloneSkill : MonoBehaviour
 {
+
+
+    public float skillCooldown = 60f;
+    public float cloneLifeTime = 30f;
+    public TextMeshProUGUI cooldownText;
+    public TextMeshProUGUI durationText;
+
+    private float cooldownTimer = 0f;
+    private float durationTimer = 0f;
     public GameObject clonePrefab;
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        // Cooldown
+        if (cooldownTimer > 0)
+            cooldownTimer -= Time.deltaTime;
+
+        // Clone tồn tại
+        if (durationTimer > 0)
+            durationTimer -= Time.deltaTime;
+
+        // UI Cooldown
+        if (cooldownTimer > 0)
+            cooldownText.text = "Cooldown: " + cooldownTimer.ToString("F1") + "s";
+        else
+            cooldownText.text = "Cooldown: Ready";
+
+        // UI Duration
+        if (durationTimer > 0)
+            durationText.text = "Clone: " + durationTimer.ToString("F1") + "s";
+        else
+            durationText.text = "Clone: --";
+
+        // Dùng skill
+        if (Input.GetKeyDown(KeyCode.K) && cooldownTimer <= 0)
         {
             GameObject clone = Instantiate(
                 clonePrefab,
@@ -15,7 +47,10 @@ public class CloneSkill : MonoBehaviour
 
             clone.GetComponent<CloneFollow>().player = transform;
 
-            Destroy(clone,10f);
+            Destroy(clone, cloneLifeTime);
+
+            cooldownTimer = skillCooldown;
+            durationTimer = cloneLifeTime;
         }
     }
 }
