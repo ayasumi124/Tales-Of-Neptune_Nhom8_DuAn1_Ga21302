15051ba843e2f493cfc2cff0a347bc1899671904
Phuc Nguyen <nnphucnguyen12@gmail.com>
2026-07-14 15:48:04 +0700

    Update CloneFollow.cs

--- Assets/Scripts/Skills/CloneFollow.cs
+++ Assets/Scripts/Skills/CloneFollow.cs
@@ -47,6 +47,7 @@ public class CloneFollow : MonoBehaviour
 
     void Start()
     {
+        Debug.Log("CloneFollow script started.");
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
