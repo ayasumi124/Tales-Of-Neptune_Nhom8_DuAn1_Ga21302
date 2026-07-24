1730842887190a9f9752512111dd25eee9889d92
Phuc Nguyen <nnphucnguyen12@gmail.com>
2026-07-24 15:17:59 +0700

    update chest

--- Assets/Scripts/Items/Chest/Chest.cs
+++ Assets/Scripts/Items/Chest/Chest.cs
@@ -4,13 +4,27 @@ public class Chest : MonoBehaviour
 {
     public AbilityData reward;
 
-    bool opened;
+    [SerializeField]
+    private bool opened;
+    bool rewardGiven = false;
+    Players player;
 
+    EnermyMovement[] enemies;
+
+    CloneFollow[] clones;
     Animator animator;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        player = FindFirstObjectByType<Players>();
+
+        enemies = FindObjectsByType<EnermyMovement>(
+            FindObjectsSortMode.None);
+
+        clones = FindObjectsByType<CloneFollow>(
+            FindObjectsSortMode.None);
+        SkillUnlockUI.OnSkillPanelClosed += ResumeGame;
     }
 
     void OnTriggerStay2D(Collider2D other)
@@ -23,15 +37,88 @@ public class Chest : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            opened = true;
-
-            animator.SetTrigger("Open");
+            OpenChest();
         }
+    }
+
+    public void OpenChest()
+    {
+        if (opened)
+            return;
+
+        opened = true;
+
+        FreezeGame();
+
+        animator.SetBool("IsOpened", true);
+    }
+    void FreezeGame()
+    {
+        player.enabled = false;
+
+        foreach (EnermyMovement e in enemies)
+        {
+            if (e != null)
+            {
+                e.enabled = false;
+
+                Rigidbody2D rb = e.GetComponent<Rigidbody2D>();
+
+                if (rb != null)
+                    rb.linearVelocity = Vector2.zero;
+            }
+        }
+
+        foreach (CloneFollow c in clones)
+        {
+            if (c != null)
+            {
+                c.enabled = false;
+
+                Rigidbody2D rb = c.GetComponent<Rigidbody2D>();
+
+                if (rb != null)
+                    rb.linearVelocity = Vector2.zero;
+            }
+        }
+    }
+
+    void ResumeGame()
+    {
+        player.enabled = true;
+
+        enemies = FindObjectsByType<EnermyMovement>(
+            FindObjectsSortMode.None);
+
+        foreach (EnermyMovement e in enemies)
+        {
+            if (e != null)
+                e.enabled = true;
+        }
+
+        clones = FindObjectsByType<CloneFollow>(
+            FindObjectsSortMode.None);
+
+        foreach (CloneFollow c in clones)
+        {
+            if (c != null)
+                c.enabled = true;
+        }
+    }
+
+    void OnDestroy()
+    {
+        SkillUnlockUI.OnSkillPanelClosed -= ResumeGame;
     }
 
     // Animation Event
     public void GiveReward()
     {
+        if (rewardGiven)
+            return;
+
+        rewardGiven = true;
+
         AbilityManager.Instance.UnlockAbility(reward);
 
         SkillUnlockUI.Instance.ShowSkill(reward);
--- Assets/Scripts/UI/SkillUnlockUI.cs
+++ Assets/Scripts/UI/SkillUnlockUI.cs
@@ -1,44 +1,53 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System;
 
 public class SkillUnlockUI : MonoBehaviour
 {
+    public static SkillUnlockUI Instance;
 
-      public static SkillUnlockUI Instance;
+    public static Action OnSkillPanelClosed;
 
-void Awake()
-{
-    Instance = this;
-}
-
-void Start()
-{
-    gameObject.SetActive(false);
-}
     public Image icon;
     public TextMeshProUGUI skillName;
     public TextMeshProUGUI description;
 
+    bool waitingForClose;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!waitingForClose)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            waitingForClose = false;
+
+            gameObject.SetActive(false);
+
+            OnSkillPanelClosed?.Invoke();
+        }
+    }
 
     public void ShowSkill(AbilityData data)
-{
-    gameObject.SetActive(true);
+    {
+        gameObject.SetActive(true);
 
-    icon.sprite = data.icon;
-    skillName.text = data.skillName;
-    description.text = data.description;
+        icon.sprite = data.icon;
+        skillName.text = data.skillName;
+        description.text = data.description;
 
-    StopAllCoroutines();
-    StartCoroutine(HideUI());
-}
-
-IEnumerator HideUI()
-{
-    yield return new WaitForSeconds(3f);
-
-    gameObject.SetActive(false);
-}
-
+        waitingForClose = true;
+    }
 }
