ab1123cc172170b6e312c710bc9eae508c43d12e
Phuc Nguyen <nnphucnguyen12@gmail.com>
2026-07-28 12:15:44 +0700

    n

--- Assets/Scripts/Scene/GameManager.cs
+++ Assets/Scripts/Scene/GameManager.cs
@@ -28,19 +28,24 @@ public class GameManager : MonoBehaviour
     }
 
     public void MovePlayerToSpawn()
-{
-    SpawnPoint[] points = FindObjectsByType<SpawnPoint>(FindObjectsSortMode.None);
-
-    foreach (SpawnPoint p in points)
     {
-        if (p.spawnID == nextSpawnPoint)
-        {
-            player.transform.position = p.transform.position;
-            Debug.Log("Spawn tại: " + p.spawnID);
-            return;
-        }
-    }
+        Debug.Log("Spawn cần tìm: " + nextSpawnPoint);
 
-    Debug.LogWarning("Không tìm thấy SpawnPoint: " + nextSpawnPoint);
-}
+        SpawnPoint[] points = FindObjectsByType<SpawnPoint>(FindObjectsSortMode.None);
+
+        foreach (SpawnPoint p in points)
+        {
+            Debug.Log("Spawn hiện có: " + p.spawnID);
+
+            if (p.spawnID == nextSpawnPoint)
+            {
+                player.transform.position = p.transform.position;
+                Debug.Log(player.transform.position);
+                Debug.Log("Đã spawn tại " + p.spawnID);
+                return;
+            }
+        }
+
+        Debug.LogError("KHÔNG TÌM THẤY SPAWN!");
+    }
 }
