9063970427a4b642028f0006b2ed31b6b6464b74
Phuc Nguyen <nnphucnguyen12@gmail.com>
2026-08-15 12:50:21 +0700

    update

--- Assets/Scripts/Scene/SpawnPoint/StartingSpawn.cs
+++ Assets/Scripts/Scene/SpawnPoint/StartingSpawn.cs
@@ -6,11 +6,23 @@ public class StartingSpawn : MonoBehaviour
     [SerializeField] private string startingSpawnID =
         "Spawn_Start";
 
+    [SerializeField] private string gameplayScene =
+        "Valley";
+
     private void Start()
     {
         if (GameManager.Instance == null)
             return;
 
+        // Không spawn Player trong MainMenu
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+            return;
+
+        // Chỉ áp dụng StartingSpawn cho scene gameplay đầu tiên
+        if (SceneManager.GetActiveScene().name != gameplayScene)
+            return;
+
+        // Nếu đã có spawn trước đó thì không ghi đè
         if (!string.IsNullOrWhiteSpace(
             GameManager.Instance.CurrentSpawnID))
         {
